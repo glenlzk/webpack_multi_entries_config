531e5d7f62686431e2681ab382849ba72fbf8c73
glen <512043920@qq.com>
2018-01-13 19:24:55 +0800

    demo3 submit

---
+++ demo03/src/assets/js/temp.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/12/29 0029.
+ */
+
+exports.name = function () {
+    return 'temp_name'
+};
+exports.game = function () {
+    return 'temp_game'
+};
+
+/*module.exports = function () {
+    return 'temp_name'
+};*/
+
+
---
+++ demo03/src/html/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lenovo on 2017/12/27.
+ */
+
+console.log('index');
+
+const calFun = () => {
+    return 'biebiebie....';
+};
+
+console.log(calFun());
